3ba3be3b57486ecb874ca8352f377f6217914062
David Vassett <davidvassett@gmail.com>
2022-03-30 16:49:17 +1100

    configure mui theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { ThemeProvider } from "@mui/material/styles";
+import { addDecorator } from "@storybook/react";
+import theme from "../src/utils/theme";
+
+addDecorator((story) => <ThemeProvider theme={theme}>{story()}</ThemeProvider>);
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +12,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};

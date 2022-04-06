9952aff4f1416d6dee77e41c77e376f125b2cf29
Hivemind-James <36725809+Hivemind-James@users.noreply.github.com>
2022-04-06 14:35:45 +1000

    feat(Typography): add typography component (#1)

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,10 @@
 module.exports = {
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/react",
+  features: {
+    emotionAlias: false,
+  },
+  staticDirs: ["./assets/fonts"],
   stories: [
     "../src/components/**/*.stories.mdx",
     "../src/components/**/*.stories.@(js|jsx|ts|tsx)",

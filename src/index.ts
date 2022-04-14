01846615eeaab911e62c7c37cee4e09c794dd05d
James <jamesglew@gmail.com>
2022-04-14 11:12:13 +1000

    build: wip

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+export { Button } from "./components/Button";
 export * from "./components/Button";
+export { Typography } from "./components/Typography";
 export * from "./components/Typography";
 export { default as theme } from "./utils/theme";

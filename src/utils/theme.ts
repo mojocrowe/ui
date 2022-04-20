aa60e15d59f9b4046f656b5820767bcab046cbf5
James <jamesglew@gmail.com>
2022-04-20 10:13:20 +1000

    build: wip

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,5 +1,41 @@
 import { createTheme } from "@mui/material/styles";
 
+declare module "@mui/material/styles" {
+  interface Palette {
+    tertiary: Palette["primary"];
+  }
+
+  interface PaletteOptions {
+    tertiary?: PaletteOptions["primary"];
+  }
+
+  interface PaletteColor {
+    700?: string;
+    600?: string;
+    500?: string;
+    400?: string;
+    300?: string;
+    200?: string;
+    100?: string;
+    50?: string;
+    25?: string;
+    0?: string;
+  }
+
+  interface SimplePaletteColorOptions {
+    700?: string;
+    600?: string;
+    500?: string;
+    400?: string;
+    300?: string;
+    200?: string;
+    100?: string;
+    50?: string;
+    25?: string;
+    0?: string;
+  }
+}
+
 const theme = createTheme({
   palette: {
     primary: {

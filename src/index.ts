b5e3a623330832b975a3569b425cec71d27a91e4
Hivemind-James <36725809+Hivemind-James@users.noreply.github.com>
2022-06-02 14:08:08 +1000

    build(rollup): bundle the ui library (#4)

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as Button } from "./components/Button/Button";
+
+export { default as Typography } from "./components/Typography/Typography";
+
+export { default as theme } from "./utils/theme";
